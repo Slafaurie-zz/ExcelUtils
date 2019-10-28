6d59ce9d4dd903771239b848e51f37df15320ace
Slafaurie <57000976+Slafaurie@users.noreply.github.com>
2019-10-28 06:08:13 -0500

    Add files via upload

---
+++ abrir_archivo_0.0.0.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 11:30:51 2019
+
+@author: Sebastian Lafaurie
+"""
+import pandas as pd 
+
+''' Descripcion: Abre un DataFrame a partir de un archivo csv o xlsx teniendo en cuenta si el separador es ; o ,'''
+
+def abrir_df(archivo, indice = None):
+    
+    formato = archivo.split(".")[-1]
+    
+    
+    if formato == 'xlsx':
+        
+        df = pd.read_excel(archivo, dtype = str)
+        
+    elif formato == 'csv': 
+        
+        df = pd.read_csv(archivo, nrows = 1)
+    
+        if len(df.columns) > 1: 
+            
+            df = pd.read_csv(archivo, dtype = str)
+            
+        else: 
+            
+            df = pd.read_csv(archivo, sep = ";", dtype = str)
+            
+    else: 
+        
+        df = pd.read_csv(archivo, nrows = 1, sep = " ")
+        
+        if len(df.columns) > 1: 
+            
+            df = pd.read_csv(archivo, sep = " ", dtype = str)
+        
+        else: 
+            
+            df = pd.read_csv(archivo, sep = ",")
+        
+    
+    if not indice: 
+        
+        return df
+        
+    else: 
+        
+        df = df.set_index(indice)
+        
+        return df
---
+++ eliminar_duplicados_0.0.0.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 11:32:21 2019
+
+@author: Aspen HYSYS
+"""
+
+import numpy as np
+
+def eliminar_dup_xy(df): 
+    
+    y_col = [x for x in df.columns if x.endswith("_x")]
+    
+    x_col = [x for x in df.columns if x.endswith("_y")]
+    
+    col_new = [x.split("_")[0] for x in x_col]
+
+    x_col.sort(), y_col.sort(), col_new.sort()
+
+    for column in range(len(col_new)):
+
+        df[col_new[column]] = np.where(df[x_col[column]].isnull(), df[y_col[column]], df[x_col[column]]) 
+
+    # ELIMINAR x_col  e y_col  -------------------------------------------------
+
+    df.drop(x_col, axis=1, inplace=True)
+    df.drop(y_col, axis=1, inplace=True)   
+
+    # ELIMINAR DUPLICADOS  -------------------------------------------------
+
+
+    df = df.loc[~df.index.duplicated(keep = "first")] 
+    
+    return df
